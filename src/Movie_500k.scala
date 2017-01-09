67a79a0b1c0e046bce040e0230e8bea9802e38db
rajduwluk <drajendr@usc.edu>
2017-01-09 02:31:55 -0500

    Update Movie_500k.scala

--- src/Movie_500k.scala
+++ src/Movie_500k.scala
@@ -1,4 +1,4 @@
-package com.sundogsoftware.spark
+package com.AmaxonReviews
 
 
 import scala.concurrent.{ Await, Future }
@@ -11,16 +11,15 @@ import scala.io.Codec
 import scala.math.sqrt
 
 
-// To run on EMR successfully + output results for Star Wars:
-// aws s3 cp s3://sundog-spark/MovieSimilarities1M.jar ./
-// aws s3 cp s3://sundog-spark/ml-1m/movies.dat ./
-// spark-submit --executor-memory 1g MovieSimilarities1M.jar 260
+// To run on AWS EMR successfully
+// Alternatively it could have been run on a EC2 compute cluster as well
+// spark-submit --executor-memory 1g ReviewSimilarities1M.jar 260
 
 
-object MovieSimilarities1M {
+object ReviewSimilarities1M {
   
-  /** Load up a Map of movie IDs to movie names. */
-  def loadMovieNames() : Map[Int, String] = {
+  /** Load up a Map of Review IDs to Review names. */
+  def loadReviewNames() : Map[Int, String] = {
     
     // Handle character encoding issues:
     implicit val codec = Codec("UTF-8")
@@ -28,41 +27,41 @@ object MovieSimilarities1M {
     codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
 
     // Create a Map of Ints to Strings, and populate it from u.item.
-    var movieNames:Map[Int, String] = Map()
+    var ReviewNames:Map[Int, String] = Map()
     
-     val lines = Source.fromFile("movies.dat").getLines()
+     val lines = Source.fromFile("Reviews.dat").getLines()
      for (line <- lines) {
        var fields = line.split("::")
        if (fields.length > 1) {
-        movieNames += (fields(0).toInt -> fields(1))
+        ReviewNames += (fields(0).toInt -> fields(1))
        }
      }
     
-     return movieNames
+     return ReviewNames
   }
   
-  type MovieRating = (Int, Double)
-  type UserRatingPair = (Int, (MovieRating, MovieRating))
+  type ReviewRating = (Int, Double)
+  type UserRatingPair = (Int, (ReviewRating, ReviewRating))
   def makePairs(userRatings:UserRatingPair) = {
-    val movieRating1 = userRatings._2._1
-    val movieRating2 = userRatings._2._2
+    val ReviewRating1 = userRatings._2._1
+    val ReviewRating2 = userRatings._2._2
     
-    val movie1 = movieRating1._1
-    val rating1 = movieRating1._2
-    val movie2 = movieRating2._1
-    val rating2 = movieRating2._2
+    val Review1 = ReviewRating1._1
+    val rating1 = ReviewRating1._2
+    val Review2 = ReviewRating2._1
+    val rating2 = ReviewRating2._2
     
-    ((movie1, movie2), (rating1, rating2))
+    ((Review1, Review2), (rating1, rating2))
   }
   
   def filterDuplicates(userRatings:UserRatingPair):Boolean = {
-    val movieRating1 = userRatings._2._1
-    val movieRating2 = userRatings._2._2
+    val ReviewRating1 = userRatings._2._1
+    val ReviewRating2 = userRatings._2._2
     
-    val movie1 = movieRating1._1
-    val movie2 = movieRating2._1
+    val Review1 = ReviewRating1._1
+    val Review2 = ReviewRating2._1
     
-    return movie1 < movie2
+    return Review1 < Review2
   }
   
   type RatingPair = (Double, Double)
@@ -104,74 +103,75 @@ object MovieSimilarities1M {
     // Create a SparkContext without much actual configuration
     // We want EMR's config defaults to be used.
     val conf = new SparkConf()
-    conf.setAppName("MovieSimilarities1M")
+    conf.setAppName("ReviewSimilarities1M")
     val sc = new SparkContext(conf)
     
-    println("\nLoading movie names...")
-    val nameDict = loadMovieNames()
-    
-    val data = sc.textFile("s3n://sundog-spark/ml-1m/ratings.dat")
+    println("\nLoading Review names...")
+    val nameDict = loadReviewNames()
+ // I have omitted the file path for company privacy reasons   
+    val data = sc.textFile("s3n://.dat")
 
-    // Map ratings to key / value pairs: user ID => movie ID, rating
+    // Map ratings to key / value pairs: user ID => Review ID, rating
     val ratings = data.map(l => l.split("::")).map(l => (l(0).toInt, (l(1).toInt, l(2).toDouble)))
     
-    // Emit every movie rated together by the same user.
+    // Emit every Review rated together by the same user.
     // Self-join to find every combination.
     val joinedRatings = ratings.join(ratings)   
     
-    // At this point our RDD consists of userID => ((movieID, rating), (movieID, rating))
+    // At this point our RDD consists of userID => ((ReviewID, rating), (ReviewID, rating))
 
     // Filter out duplicate pairs
     val uniqueJoinedRatings = joinedRatings.filter(filterDuplicates)
 
-    // Now key by (movie1, movie2) pairs.
-    val moviePairs = uniqueJoinedRatings.map(makePairs).partitionBy(new HashPartitioner(100))
+    // Now key by (Review1, Review2) pairs.
+    val ReviewPairs = uniqueJoinedRatings.map(makePairs).partitionBy(new HashPartitioner(100))
 
-    // We now have (movie1, movie2) => (rating1, rating2)
-    // Now collect all ratings for each movie pair and compute similarity
-    val moviePairRatings = moviePairs.groupByKey()
+    // We now have (Review1, Review2) => (rating1, rating2)
+    // Now collect all ratings for each Review pair and compute similarity
+    val ReviewPairRatings = ReviewPairs.groupByKey()
 
-    // We now have (movie1, movie2) = > (rating1, rating2), (rating1, rating2) ...
+    // We now have (Review1, Review2) = > (rating1, rating2), (rating1, rating2) ...
     // Can now compute similarities.
-    val moviePairSimilarities = moviePairRatings.mapValues(computeCosineSimilarity).cache()
+    val ReviewPairSimilarities = ReviewPairRatings.mapValues(computeCosineSimilarity).cache()
     
     //Save the results if desired
-    //val sorted = moviePairSimilarities.sortByKey()
-    //sorted.saveAsTextFile("movie-sims")
+    //val sorted = ReviewPairSimilarities.sortByKey()
+    //sorted.saveAsTextFile("Review-sims")
     
-    // Extract similarities for the movie we care about that are "good".
+    // Extract similarities for the Review we care about that are "good".
     
     if (args.length > 0) {
       val scoreThreshold = 0.97
       val coOccurenceThreshold = 1000.0
       
-      val movieID:Int = args(0).toInt
+      val ReviewID:Int = args(0).toInt
       
-      // Filter for movies with this sim that are "good" as defined by
+      // Filter for Reviews with this sim that are "good" as defined by
       // our quality thresholds above     
       
-      val filteredResults = moviePairSimilarities.filter( x =>
+      val filteredResults = ReviewPairSimilarities.filter( x =>
         {
           val pair = x._1
           val sim = x._2
-          (pair._1 == movieID || pair._2 == movieID) && sim._1 > scoreThreshold && sim._2 > coOccurenceThreshold
+          (pair._1 == ReviewID || pair._2 == ReviewID) && sim._1 > scoreThreshold && sim._2 > coOccurenceThreshold
         }
       )
         
       // Sort by quality score.
       val results = filteredResults.map( x => (x._2, x._1)).sortByKey(false).take(50)
       
-      println("\nTop 50 similar movies for " + nameDict(movieID))
+      println("\nTop 50 similar Reviews for " + nameDict(ReviewID))
       for (result <- results) {
         val sim = result._1
         val pair = result._2
-        // Display the similarity result that isn't the movie we're looking at
-        var similarMovieID = pair._1
-        if (similarMovieID == movieID) {
-          similarMovieID = pair._2
+        // Display the similarity result that isn't the Review we're looking at
+        var similarReviewID = pair._1
+        if (similarReviewID == ReviewID) {
+          similarReviewID = pair._2
         }
-        println(nameDict(similarMovieID) + "\tscore: " + sim._1 + "\tstrength: " + sim._2)
+        println(nameDict(similarReviewID) + "\tscore: " + sim._1 + "\tstrength: " + sim._2)
       }
     }
   }
 }
+
